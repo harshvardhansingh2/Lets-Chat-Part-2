0bdb57b3e900af156d4e20006d169a61baa9e9ab
harshvardhansingh2 <97771241+harshvardhansingh2@users.noreply.github.com>
2022-02-22 17:49:00 -0600

    Add files via upload

---
+++ LetsChat_room.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAc2Oj2vmbwMPSxgAbvhtMyx5A_bjC9cWg",
+    authDomain: "letschat-3ce0c.firebaseapp.com",
+    databaseURL: "https://letschat-3ce0c-default-rtdb.firebaseio.com",
+    projectId: "letschat-3ce0c",
+    storageBucket: "letschat-3ce0c.appspot.com",
+    messagingSenderId: "749114176564",
+    appId: "1:749114176564:web:e5b456f127c0c3735bc661"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
